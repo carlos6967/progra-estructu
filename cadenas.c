b727ac36fe511f4c3e192d24dddcb8b14a3a4349
carlos6967 <carlos.bustamante1987@alumnos.udg.mx>
2026-03-13 22:05:33 -0600

    Update cadenas.c

--- cadenas.c
+++ cadenas.c
@@ -8,7 +8,12 @@ char main(){
     char name3[20];
     char nombres[2][20];
     int i;
+    char origen[16];
     char copia1[16];
+    char nombre[20];
+    char apellido[20];
+    char a[20];
+    char b[20];
 
     /*printf("%s\n", name1);
 
@@ -23,7 +28,7 @@ char main(){
     strcpy(jugadores[2], "Carlos");
 
     printf("%s", jugadores[2]);
-*/
+
     for(i=0; i<3; i++){
         fgets(nombres[i], sizeof(nombres[i]), stdin);
     }
@@ -34,12 +39,40 @@ char main(){
         printf("%d. %s", i + 1, nombres[i]);
     }
 
-  */  for(i=0; i<3; i++){
+    for(i=0; i<3; i++){
     printf("%d\n", strlen(nombres[i]));
     }
 
-   /* strcpy(name1, copia1);
+
+    printf("escribe la cadena que desees copiar:\n");
+    fgets(origen, sizeof(origen), stdin);
+    strcpy(copia1, origen);
     printf("%s", copia1);
-*/
-    return 0;
+
+
+    
+
+    printf("escribe tu nombre\n");
+    scanf("%s", &nombre);
+    printf("escribe tu apellido\n");
+    scanf("%s", &apellido);
+    strcat(nombre, apellido);
+    printf("tu nombre y apellido es: %s \n", nombre);
+
+    */
+    
+
+    printf("escribe una contrasena:\n");
+    fgets(a, sizeof(a), stdin);
+    printf("confirma la contrasena:\n");
+    fgets(b, sizeof(b), stdin);
+    
+    if (strcmp(a, b)==0)
+    {
+        printf("si es la misma");
+    }else{
+        printf("te equivocaste");
+    }
+
+    return 0; 
 }
