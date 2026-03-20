36dca352d7dacfc0f5cac67c9485f2ce9afe5ef7
carlos6967 <carlos.bustamante1987@alumnos.udg.mx>
2026-03-20 10:22:53 -0600

    Add files via upload

--- 20 de marzo/Untitled1.c
+++ 20 de marzo/Untitled1.c
@@ -1,17 +1,66 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<string.h>
 
 int main(){
+    FILE *archivo;
+    archivo = fopen("tablero.txt", "w");
 
     int i, j;
-    char nombre[4][20];
+    char tablero[5][5];
+    char nombre[20];
+    char asd[6];
 
-    for(i=0; i<4; i++){
-        fgets(nombre[i], sizeof(nombre[i]), stdin);
-    }
-    //for(i=0; i<50; i++){
-        for(i=0; i<4; i++){
-            printf("%d\n", &nombre[i]);
+
+
+    for(i=0; i<5; i++){
+        for(j=0; j<5; j++){
+            tablero[i][j]='~';
         }
-    //}
+    }
+
+    tablero[1][2]='B';
+    tablero[3][4]='X';
+    tablero[2][1]='O';
+
+    for(i=0; i<5; i++){
+        for(j=0; j<5; j++){
+            printf("%c ", tablero[i][j]);
+        }
+        printf("\n");
+    }
+
+    for(i=0; i<5; i++){
+        for(j=0; j<5; j++){
+            fprintf(archivo,"%c ", tablero[i][j]);
+        }
+        fprintf(archivo, "\n");
+    }
+
+    fgets(nombre, sizeof(nombre), stdin);
+    fprintf(archivo,"%s", nombre);
+
+    fprintf(archivo, "%s", nombre);
+
+
+    fclose(archivo);
+
+    fopen("tablero.txt", "r");
+
+    if (asd == fgets(asd, 7, archivo)) {
+      if (asd == "carlos") {
+         printf("%s", asd);
+        }
+        else if(asd != "carlos") {
+
+        }
+    }
+    for(i=0; i<11; i++){
+        fgets(asd, 7, archivo);
+    }
+    printf("%s", asd);
+
+    fclose(archivo);
+
+    return 0;
 }
