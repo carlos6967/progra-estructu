62020035864c8151282f03bd155810c5bce00ff3
carlos6967 <carlos.bustamante1987@alumnos.udg.mx>
2026-05-22 20:23:39 -0600

    Add files via upload

---
+++ Actividad_04_operadores_relacionales/codigo/numeroimpar.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+
+    int a;
+
+    printf("dime un n%cmero\n", 163);
+    scanf("%d",&a);
+
+    if(a % 2 != 0){
+        printf("es impar");
+    }
+    else{
+        printf("es par");
+    }
+}
